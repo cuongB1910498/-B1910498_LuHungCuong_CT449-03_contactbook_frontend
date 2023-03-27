bb59a624bad4a5166dab09308005c01e6b05f6f1
Cuong B1910498 <Cuongb1910498@student.ctu.edu.vn>
2023-03-27 11:09:34 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -43,6 +43,15 @@
                 <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link
+                    :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                    }"
+                >
+                    <span class="mt-2 badge badge-warning">
+                    <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -138,7 +147,7 @@ export default {
 <style scoped>
     .page {
         text-align: left;
-        max-width: 750px;
+        max-width: 1400px;
     }
 </style>
     
